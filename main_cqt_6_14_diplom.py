0a74fa4aa9c04a9b7545f98fc9c3dfbdd7ae642d
Nikolay Gut <nickgut@list.ru>
2024-05-18 22:31:46 +0300

    trying to visualize NN 32

--- main_cqt_6_14_diplom.py
+++ main_cqt_6_14_diplom.py
@@ -305,6 +305,16 @@ def test(val_loader, model, criterion, criterion_cent, epoch, use_cuda, file_nam
             conv_layer = module
             break  # Прерываем цикл после нахождения первого сверточного слоя
 
+    if conv_layer is not None:
+            weights = conv_layer.weight.data
+            print("Weights of the convolutional layer:")
+            print(weights.size())
+
+            x = torch.randn((1,) + tuple(inputs.shape[1:])).requires_grad_(True)
+            y = model(x)
+            dot = make_dot(y, params=dict(list(model.named_parameters()) + [('input', x)]))
+            dot.render("neural_network_graph", format="png")  # Сохраняем граф в файл
+
     for batch_idx, ss in enumerate(val_loader):
         
         melspectrogram  = ss[1].unsqueeze(1)
@@ -330,16 +340,6 @@ def test(val_loader, model, criterion, criterion_cent, epoch, use_cuda, file_nam
         losses.update(loss.item(), inputs.size(0))
         top1.update(prec1[0].item(), inputs.size(0))
         dictMeter.update(dict_test)     
-
-        if conv_layer is not None:
-            weights = conv_layer.weight.data
-            print("Weights of the convolutional layer:")
-            print(weights.size())
-
-            x = torch.randn((1,) + tuple(inputs.shape[1:])).requires_grad_(True)
-            y = model(x)
-            dot = make_dot(y, params=dict(list(model.named_parameters()) + [('input', x)]))
-            dot.render("neural_network_graph", format="png")  # Сохраняем граф в файл
 
         if batch_idx % 10 == 0:
             print('test: {}/top1: {}/loss: {}'.format(batch_idx, top1.avg, losses.avg))
