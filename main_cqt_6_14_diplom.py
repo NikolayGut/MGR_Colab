dabcef272a0e87b095f240bd99e1ae14aeb346e9
Nikolay Gut <nickgut@list.ru>
2024-05-18 21:45:53 +0300

    trying to visualize NN 20

--- main_cqt_6_14_diplom.py
+++ main_cqt_6_14_diplom.py
@@ -333,8 +333,7 @@ def test(val_loader, model, criterion, criterion_cent, epoch, use_cuda, file_nam
         print("Веса сверточного слоя:")
         print(weights)
 
-
-        x = torch.randn(1, 3, 227, 227).requires_grad_(True)
+        x = torch.randn(1, 1, weights).requires_grad_(True)
         y = model(x)
         dot = make_dot(y, params=dict(list(model.named_parameters()) + [('input', x)]))
 
